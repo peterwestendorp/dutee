dfcf8e8e91beb19c555901a8e660510179f21226
Peter Westendorp <peter@mintdigital.com>
2013-09-10 21:35:18 +0100

    add custom facebook login button

--- public/js/facebook-login.js
+++ public/js/facebook-login.js
@@ -11,31 +11,33 @@ window.fbAsyncInit = function() {
   // for any authentication related change, such as login, logout or session refresh. This means that
   // whenever someone who was previously logged out tries to log in again, the correct case below
   // will be handled.
-  FB.Event.subscribe('auth.authResponseChange', function(response) {
-    // Here we specify what we do with the response anytime this event occurs.
-    if (response.status === 'connected') {
-      // The response object is returned with a status field that lets the app know the current
-      // login status of the person. In this case, we're handling the situation where they
-      // have logged in to the app.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // In this case, the person is logged into Facebook, but not into the app, so we call
-      // FB.login() to prompt them to do so.
-      // In real-life usage, you wouldn't want to immediately prompt someone to login
-      // like this, for two reasons:
-      // (1) JavaScript created popup windows are blocked by most browsers unless they
-      // result from direct interaction from people using the app (such as a mouse click)
-      // (2) it is a bad experience to be continually prompted to login upon page load.
-      FB.login();
-    } else {
-      // In this case, the person is not logged into Facebook, so we call the login()
-      // function to prompt them to do so. Note that at this stage there is no indication
-      // of whether they are logged into the app. If they aren't then they'll see the Login
-      // dialog right after they log in to Facebook.
-      // The same caveats as above apply to the FB.login() call here.
-      FB.login();
-    }
-  });
+
+
+  // FB.Event.subscribe('auth.authResponseChange', function(response) {
+  //   // Here we specify what we do with the response anytime this event occurs.
+  //   if (response.status === 'connected') {
+  //     // The response object is returned with a status field that lets the app know the current
+  //     // login status of the person. In this case, we're handling the situation where they
+  //     // have logged in to the app.
+  //     testAPI();
+  //   } else if (response.status === 'not_authorized') {
+  //     // In this case, the person is logged into Facebook, but not into the app, so we call
+  //     // FB.login() to prompt them to do so.
+  //     // In real-life usage, you wouldn't want to immediately prompt someone to login
+  //     // like this, for two reasons:
+  //     // (1) JavaScript created popup windows are blocked by most browsers unless they
+  //     // result from direct interaction from people using the app (such as a mouse click)
+  //     // (2) it is a bad experience to be continually prompted to login upon page load.
+  //     FB.login();
+  //   } else {
+  //     // In this case, the person is not logged into Facebook, so we call the login()
+  //     // function to prompt them to do so. Note that at this stage there is no indication
+  //     // of whether they are logged into the app. If they aren't then they'll see the Login
+  //     // dialog right after they log in to Facebook.
+  //     // The same caveats as above apply to the FB.login() call here.
+  //     FB.login();
+  //   }
+  // });
 };
 
 // Load the SDK asynchronously
@@ -49,10 +51,33 @@ window.fbAsyncInit = function() {
 
 // Here we run a very simple test of the Graph API after login is successful.
 // This testAPI() function is only called in those cases.
-function testAPI() {
-  console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
-    console.log(response);
-    console.log('Good to see you, ' + response.name + '.');
+// function testAPI() {
+//   console.log('Welcome!  Fetching your information.... ');
+//   FB.api('/me', function(response) {
+//     console.log(response);
+//     console.log('Good to see you, ' + response.name + '.');
+//   });
+// }
+var facebookButton = document.getElementById("facebook-login");
+
+facebookButton.addEventListener('click', function(){
+  FB.login(function(response) {
+      if (response.authResponse) {
+          console.log('Welcome!  Fetching your information.... ');
+          // access_token = response.authResponse.accessToken;
+          // user_id = response.authResponse.userID;
+
+          FB.api('/me', function(response) {
+            console.log(response);
+            // user_email = response.email;
+          });
+
+      }
+      else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+  },
+  {
+    scope: 'publish_stream, email'
   });
-}
+});
