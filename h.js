dd0bef4417eaa079f0b66e08c77bbe08d3dd289a
Peter Westendorp <p.westendorp@afas.nl>
2017-02-18 20:36:06 +0100

    extend gitignore

--- h.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.h = function (selector, properties, getContent) {
-    return {
-        selector: selector,
-        properties: properties,
-        getContent: getContent
-    };
-};
-//# sourceMappingURL=h.js.map
