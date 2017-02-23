1eb0e5da43b3fa4656c7b5af710b0da779539e8c
Peter Westendorp <PWE@afasgroep.nl>
2017-02-23 17:29:05 +0100

    enable sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,31 @@
 module.exports = {
-    entry: "./example.ts",
-    output: {
-        filename: "bundle.js"
-    },
-    resolve: {
-        // Add '.ts' and '.tsx' as a resolvable extension.
-        extensions: [".ts", ".js"]
-    },
-    module: {
-        loaders: [
-            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ]
-    }
+  entry: "./src/example.ts",
+  output: {
+    path: __dirname + '/public',
+    filename: "bundle.js"
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as a resolvable extension.
+    extensions: [".ts", ".js"]
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      {
+        enforce: 'pre',
+        test: /\.tsx?$/,
+        use: "source-map-loader"
+      }
+    ]
+  }
 }
