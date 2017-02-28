187f12c6558f0c00d0ee097571e3d87bdede8a1f
Peter Westendorp <PWE@afasgroep.nl>
2017-02-28 14:15:52 +0100

    add new item on enter

--- src/example.ts
+++ src/example.ts
@@ -1,27 +1,25 @@
 import {myVDOM, VNode, h} from "./my-vdom";
 
 let listItems: string[] = [];
-let newItemName: string | undefined;
 
-let addListItem = () => {
-  if (newItemName) {
-    listItems.push(newItemName);
-    newItemName = undefined;
+let addListItem = (name: string) => {
+  if (name) {
+    listItems.push(name);
   }
 };
 
 let updateNewItemName = (evt: KeyboardEvent) => {
-  newItemName = (evt.target as HTMLInputElement).value;
+  if (evt.key === 'Enter') {
+    addListItem((evt.target as HTMLInputElement).value);
+  }
 };
 
 let removeListItem = (index: number) => {
   listItems.splice(index, 1);
-  console.log(listItems);
 };
 
 let render = (): VNode => h('div', {}, [
   h('input', { onKeyDown: updateNewItemName }, []),
-  h('button', { onClick: addListItem }, 'add item'),
   h('ul', {}, listItems.map((item, i) => h('li', { onClick: () => removeListItem(i) }, item)))
 ]);
 
