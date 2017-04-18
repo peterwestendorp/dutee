c778928a3aae846edb493eaa056856866bed1d7f
Peter Westendorp <p.westendorp@afas.nl>
2017-04-18 23:00:47 +0200

    add firebase auth with facebook login

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,75 @@
-export interface Person {
-  name: string;
-}
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/database");
+let firebaseui = require('firebaseui');
 
-let getPerson = (person: Person): void => {
-  console.log(person.name);
-}
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDWUuggqMPPlcsc6ez_4IZFRJxFMhckBYU",
+  authDomain: "dutee.firebaseapp.com",
+  databaseURL: "https://dutee.firebaseio.com",
+  projectId: "firebase-dutee",
+  storageBucket: "firebase-dutee.appspot.com",
+  messagingSenderId: "467628975171"
+};
+firebase.initializeApp(config);
 
-getPerson({name: 'henk'});
+// FirebaseUI config.
+var uiConfig = {
+  signInSuccessUrl: '/',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    // firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>'
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+
+
+let initApp = function() {
+  firebase.auth().onAuthStateChanged(function(user: any) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      user.getToken().then(function(accessToken: any) {
+        // document.getElementById('sign-in-status').textContent = 'Signed in';
+        // document.getElementById('sign-in').textContent = 'Sign out';
+        // document.getElementById('account-details').textContent = JSON.stringify({
+        //   displayName: displayName,
+        //   email: email,
+        //   emailVerified: emailVerified,
+        //   photoURL: photoURL,
+        //   uid: uid,
+        //   accessToken: accessToken,
+        //   providerData: providerData
+        // }, null, '  ');
+      });
+    } else {
+      // User is signed out.
+      // document.getElementById('sign-in-status').textContent = 'Signed out';
+      // document.getElementById('sign-in').textContent = 'Sign in';
+      // document.getElementById('account-details').textContent = 'null';
+    }
+  }, function(error: any) {
+    console.log(error);
+  });
+};
+
+window.addEventListener('load', function() {
+  initApp()
+});
